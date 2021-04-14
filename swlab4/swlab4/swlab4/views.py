9d7a19902f78ed5c314d9d2a2eb9bc0242f58faf
ashmrz <ashkan.m10@gmail.com>
2021-04-14 16:01:35 +0430

    Add gateway API template

--- swlab4/swlab4/swlab4/views.py
+++ swlab4/swlab4/swlab4/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+
+class GatewayAPI(viewsets.ViewSet):
+    """
+    Sample input:
+
+    """
+
+    def list(self, request):
+        request_type = request.data["type"]
+        return Response("")
