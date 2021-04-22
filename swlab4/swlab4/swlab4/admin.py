515acf20ae3e6e474444239bed253ac41f63cbbe
AradM <arad.mohammadi99@gmail.com>
2021-04-22 14:55:10 +0430

    add book model

--- swlab4/swlab4/swlab4/admin.py
+++ swlab4/swlab4/swlab4/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from swlab4.swlab4.models import Client
-
+from swlab4.swlab4.models import Client, Admin, Book
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Admin)
+class AdminAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
     pass
--- swlab4/swlab4/swlab4/models.py
+++ swlab4/swlab4/swlab4/models.py
@@ -17,3 +17,11 @@ class Admin(models.Model):
     email = models.EmailField()
     token = models.CharField(max_length=256, default="")
     token_generation_time = models.DateTimeField(auto_now=True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    author = models.CharField(max_length=256)
+    publisher = models.CharField(max_length=256)
+    category = models.CharField(max_length=256)
+    book_id = models.AutoField(primary_key=True)
